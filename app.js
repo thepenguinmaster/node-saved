e64b6642b6004992f6f1fcb5c74e791f9d494136
thepenguinmaster <thepenguinmaster@msn.com>
2019-10-02 03:57:03 +0000

    Uploaded files from LOD

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-
+var learningRouter = require('./routes/learning-route');
 var indexRouter = require('./routes/index');
 var labCode = require('./lab-code');
 var app = express();
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
-
+app.use('/learn', learningRouter);
 app.use('/', indexRouter);
 app.use('/lab', labCode);
 
--- lab-code.js
+++ lab-code.js
@@ -13,7 +13,6 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/user', function(req, res, next) {
-    //res.send('Hello World');
     res.send(new User("Bob","bob@bob.com", 22) );
     res.end;
 });
@@ -22,8 +21,7 @@ router.post('/user', function(req, res, next) {
     var name = req.body.name;
     var domain = req.body.domain;
     var age = req.body.age;
-    // test2
-    // res.send(name+age+'@'+domain);
+
     res.send("Nope");
     res.end;
 });
